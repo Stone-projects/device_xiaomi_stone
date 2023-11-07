9d32e48d6fec5b05e5f815bb7a84db80c7e3839d
Agatha senpai <naufalmarjan2014@gmail.com>
2023-11-07 15:14:38 +0000

    stone: Fix TARGET_DEVICE codename after decommon

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter moonstone peux stone sunstone veux,$(TARGET_DEVICE)),)
+ifeq ($(TARGET_DEVICE),stone)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
